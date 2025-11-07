455741f9af0fea50e69fabf98f02c55a1806054c
Leeka99 <dsrn0000@gmail.com>
2025-11-07 23:42:36 +0900

    feat: Student 엔티티 구현

---
+++ api/src/main/java/com/techcourse/api/domain/Student.java
@@ -0,0 +1,27 @@
+package com.techcourse.api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int grade;
+
+    public Student() {
+
+    }
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+}
