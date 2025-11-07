bec334fd97348b2febb79b1728661c11866e5115
Leeka99 <dsrn0000@gmail.com>
2025-11-07 23:42:59 +0900

    feat: Course 엔티티 구현

---
+++ api/src/main/java/com/techcourse/api/domain/Course.java
@@ -0,0 +1,42 @@
+package com.techcourse.api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private int maxCapacity;
+
+    private int currentCapacity;
+
+    public Course() {
+
+    }
+
+    public Course(String title, int maxCapacity) {
+        this.title = title;
+        this.maxCapacity = maxCapacity;
+        this.currentCapacity = 0;
+    }
+
+    public boolean isAvailable() {
+        return currentCapacity < maxCapacity;
+    }
+
+    public void increaseCapacity() {
+        this.currentCapacity++;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
