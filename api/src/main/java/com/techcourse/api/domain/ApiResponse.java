08d0414ff59da75b8e6afd8f8ca4a2b8d33c55dc
Leeka99 <dsrn0000@gmail.com>
2025-11-08 14:25:18 +0900

    feat: API 공통 응답 포맷 추가

---
+++ api/src/main/java/com/techcourse/api/domain/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.techcourse.api.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "공통 응답 DTO", name = "ApiResponse")
+public record ApiResponse<T>(
+    @Schema(description = "서비스 정의 코드", example = "2000")
+    String code,
+    @Schema(description = "응답 메시지", example = "성공적으로 처리되었습니다.")
+    String message,
+    @Schema(description = "응답 데이터")
+    T data
+) {
+
+    public static <T> ApiResponse<T> show(T data) {
+        return new ApiResponse<>(ResponseCode.OK.code(), ResponseCode.OK.message(), data);
+    }
+
+    public static <T> ApiResponse<T> success(String message) {
+        return new ApiResponse<>(ResponseCode.OK.code(), message, null);
+    }
+}
