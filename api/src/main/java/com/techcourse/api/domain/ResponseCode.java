d165eae3693a133bce97fb4d08335a29e60fc87a
Leeka99 <dsrn0000@gmail.com>
2025-11-08 14:25:45 +0900

    feat: 응답코드 포맷 추가

---
+++ api/src/main/java/com/techcourse/api/domain/ResponseCode.java
@@ -0,0 +1,29 @@
+package com.techcourse.api.domain;
+
+import org.springframework.http.HttpStatus;
+
+public enum ResponseCode {
+    OK("200", HttpStatus.OK, "정상적으로 완료되었습니다.");
+
+    private final String code;
+    private final HttpStatus status;
+    private final String message;
+
+    ResponseCode(String code, HttpStatus status, String message) {
+        this.code = code;
+        this.status = status;
+        this.message = message;
+    }
+
+    public String code() {
+        return code;
+    }
+
+    public HttpStatus status() {
+        return status;
+    }
+
+    public String message() {
+        return message;
+    }
+}
