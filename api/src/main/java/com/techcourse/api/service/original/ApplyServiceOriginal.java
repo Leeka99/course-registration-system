6c6eeacbbc9acf6714b291736e0a282754e94108
Leeka99 <dsrn0000@gmail.com>
2025-11-08 23:09:48 +0900

    refactor: ApplyService 이름 변경
    - ApplyService -> ApplyServiceOriginal
    - 락 제어 방법에 대해서 버전별로 관리하기 위해 이름을 변경함

---
+++ api/src/main/java/com/techcourse/api/service/original/ApplyServiceOriginal.java
@@ -0,0 +1,39 @@
+package com.techcourse.api.service.original;
+
+import com.techcourse.api.domain.entity.Course;
+import com.techcourse.api.domain.entity.Registration;
+import com.techcourse.api.domain.entity.Student;
+import com.techcourse.api.repository.CourseRepository;
+import com.techcourse.api.repository.RegistrationRepository;
+import com.techcourse.api.repository.StudentRepository;
+import com.techcourse.api.service.ApplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyServiceOriginal implements ApplyService {
+
+    private final RegistrationRepository registrationRepository;
+    private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
+
+    @Override
+    public void apply(Long studentId, Long courseId) {
+        Student student = studentRepository.findById(studentId).orElseThrow();
+        Course course = courseRepository.findById(courseId).orElseThrow();
+
+        // 수강 가능 인원이 남아있다면
+        if (course.isAvailable()) {
+            course.increaseCapacity();
+            courseRepository.save(course);
+            registrationRepository.save(Registration.changeToComplete(student, course));
+            return;
+        }
+        if (!course.isAvailable()) {
+            // 가득 찼다면 대기순번으로
+            registrationRepository.save(Registration.changeToWait(student, course));
+        }
+    }
+}
+
