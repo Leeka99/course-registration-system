3c642907b0276ef9b221d5b6f27acb0b966ad518
Leeka99 <dsrn0000@gmail.com>
2025-11-08 23:07:16 +0900

    refactor: 등록 전체조회 서비스 분리
    - 기존 Apply 서비스로직에 포함되어있던 등록 전체조회 서비스를 별도 클래스로 분리

--- api/src/main/java/com/techcourse/api/service/original/ShowRegistrationService.java
+++ api/src/main/java/com/techcourse/api/service/original/ShowRegistrationService.java
@@ -1,46 +1,24 @@
-package com.techcourse.api.service;
+package com.techcourse.api.service.original;
 
-import com.techcourse.api.domain.Course;
-import com.techcourse.api.domain.Registration;
-import com.techcourse.api.domain.ShowRegistrationResponse;
-import com.techcourse.api.domain.Student;
+import com.techcourse.api.domain.dto.ShowRegistrationResponse;
+import com.techcourse.api.domain.entity.Course;
+import com.techcourse.api.domain.entity.Registration;
+import com.techcourse.api.domain.entity.Student;
 import com.techcourse.api.repository.CourseRepository;
 import com.techcourse.api.repository.RegistrationRepository;
 import com.techcourse.api.repository.StudentRepository;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ApplyService {
+@RequiredArgsConstructor
+public class ShowRegistrationService {
 
     private final RegistrationRepository registrationRepository;
     private final StudentRepository studentRepository;
     private final CourseRepository courseRepository;
-
-    public ApplyService(RegistrationRepository registrationRepository,
-        StudentRepository studentRepository, CourseRepository courseRepository) {
-        this.registrationRepository = registrationRepository;
-        this.studentRepository = studentRepository;
-        this.courseRepository = courseRepository;
-    }
-
-    public void apply(Long studentId, Long courseId) {
-        Student student = studentRepository.findById(studentId).orElseThrow();
-        Course course = courseRepository.findById(courseId).orElseThrow();
-
-        // 수강 가능 인원이 남아있다면
-        if (course.isAvailable()) {
-            course.increaseCapacity();
-            courseRepository.save(course);
-            registrationRepository.save(Registration.changeToComplete(student, course));
-            return;
-        }
-        if (!course.isAvailable()) {
-            // 가득 찼다면 대기순번으로
-            registrationRepository.save(Registration.changeToWait(student, course));
-        }
-    }
 
     public List<ShowRegistrationResponse> showRegistration() {
         List<Registration> registrations = registrationRepository.findAll();
@@ -65,6 +43,4 @@ public class ApplyService {
 
         return responses;
     }
-
 }
-
