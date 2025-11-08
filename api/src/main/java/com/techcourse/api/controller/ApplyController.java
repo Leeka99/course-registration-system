57b88e51c02724660734ff227e2cda80412d1c46
Leeka99 <dsrn0000@gmail.com>
2025-11-08 14:23:44 +0900

    feat: 전체 등록 상황 조회 컨트롤러 구현

--- api/src/main/java/com/techcourse/api/controller/ApplyController.java
+++ api/src/main/java/com/techcourse/api/controller/ApplyController.java
@@ -1,10 +1,13 @@
 package com.techcourse.api.controller;
 
 import com.techcourse.api.domain.ApiResponse;
+import com.techcourse.api.domain.ShowRegistrationResponse;
 import com.techcourse.api.service.ApplyService;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplyController {
 
     private final ApplyService applyService;
+
+    @Operation(summary = "등록 목록 전체조회", description = "전체 등록 상황을 조회합니다.")
+    @GetMapping("/show")
+    public ResponseEntity<ApiResponse<List<ShowRegistrationResponse>>> showRegistration() {
+        List<ShowRegistrationResponse> response = applyService.showRegistration();
+        return ResponseEntity.ok(ApiResponse.show(response));
+    }
 
     @Operation(summary = "수강신청", description = "수강신청을 진행합니다.")
     @PostMapping("/apply/{studentId}/{courseId}")
