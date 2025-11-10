bb8203bdeb1107e77019f87a8f52ee8329d286ab
Leeka99 <dsrn0000@gmail.com>
2025-11-10 23:21:45 +0900

    fix: 비관적 락, 낙관적 락 적용

--- api/src/main/java/com/techcourse/api/repository/CourseRepository.java
+++ api/src/main/java/com/techcourse/api/repository/CourseRepository.java
@@ -1,8 +1,23 @@
 package com.techcourse.api.repository;
 
 import com.techcourse.api.domain.entity.Course;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.Version;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select l from Course l where l.id = :id")
+    Optional<Course> findByIdWithPessimisticLock(@Param("id") Long id);
+
+    @Version
+    @Query("select l from Course l where l.id = :id")
+    Optional<Course> findByIdWithOptimisticLock(@Param("id") Long id);
 
 }
