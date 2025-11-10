0f1e58f815159ed737e1749295fc9e3ef41109a2
Leeka99 <dsrn0000@gmail.com>
2025-11-11 01:09:22 +0900

    test: ReentrantLock + 비관적 락 적용 후 멀티 프로세스 환경에서 테스트
    - 1학년 학생 80명 , 3학년 학생 80명이 동시에 접속하는 환경에서 1학년이 먼저 수강신청될 수 있는지 확인

--- api/src/main/java/com/techcourse/api/repository/StudentRepository.java
+++ api/src/main/java/com/techcourse/api/repository/StudentRepository.java
@@ -1,8 +1,15 @@
 package com.techcourse.api.repository;
 
 import com.techcourse.api.domain.entity.Student;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    @Query("select s from Student s where s.grade = 1 ")
+    List<Student> findFirstGradeStudents();
+
+    @Query("select s from Student s where s.grade != 1 ")
+    List<Student> findOtherGradeStudents();
 }
