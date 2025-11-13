6fcca43fbf79f06e73cc568ca00afaebbb8893e7
Leeka99 <dsrn0000@gmail.com>
2025-11-13 20:40:23 +0900

    refactor: 기존 ReentrantLock 서비스로 이전

--- api/src/main/java/com/techcourse/api/service/javalock/ApplyServiceReentrantLock.java
+++ api/src/main/java/com/techcourse/api/service/javalock/ApplyServiceReentrantLock.java
@@ -1,33 +1,27 @@
 package com.techcourse.api.service.javalock;
 
-import com.techcourse.api.domain.entity.Course;
-import com.techcourse.api.domain.entity.Registration;
 import com.techcourse.api.domain.entity.Student;
-import com.techcourse.api.repository.CourseRepository;
-import com.techcourse.api.repository.RegistrationRepository;
 import com.techcourse.api.repository.StudentRepository;
 import com.techcourse.api.service.ApplyService;
+import com.techcourse.api.service.RegistrationService;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ApplyServiceReentrantLock implements ApplyService {
 
-    private final RegistrationRepository registrationRepository;
     private final StudentRepository studentRepository;
-    private final CourseRepository courseRepository;
+    private final RegistrationService registrationService;
 
     private final ReentrantLock lock = new ReentrantLock();
-    private final Condition otherGradeQueue = lock.newCondition();
+    private final Condition otherGradeCondition = lock.newCondition();
     private final AtomicInteger firstYearRunning = new AtomicInteger(0);
 
     @Override
-    @Transactional
     public void apply(Long studentId, Long courseId) {
         Student student = studentRepository.findById(studentId).orElseThrow();
         lock.lock();
@@ -35,32 +29,20 @@ public class ApplyServiceReentrantLock implements ApplyService {
             if (student.getGrade() == 1) {
                 firstYearRunning.incrementAndGet();
             }
-            if (student.getGrade() != 1) {
-                while (firstYearRunning.get() > 0) {
-                    try {
-                        otherGradeQueue.await();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        return;
-                    }
+            while (student.getGrade() != 1 && firstYearRunning.get() > 0) {
+                try {
+                    otherGradeCondition.await();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
                 }
             }
 
-            Course course = courseRepository.findById(courseId).orElseThrow();
-
-            if (course.isAvailable()) {
-                course.increaseCapacity();
-                courseRepository.save(course);
-                registrationRepository.save(Registration.changeToComplete(student, course));
-            } else {
-                registrationRepository.save(Registration.changeToWait(student, course));
-            }
+            registrationService.courseRegistration(student, courseId);
 
             if (student.getGrade() == 1) {
                 firstYearRunning.decrementAndGet();
-                if (firstYearRunning.get() == 0) {
-                    otherGradeQueue.signalAll();
-                }
+                otherGradeCondition.signalAll();
             }
         } finally {
             lock.unlock();
