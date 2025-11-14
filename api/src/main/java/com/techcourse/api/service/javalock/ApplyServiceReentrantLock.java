24cf6cba71a31f108dbb7394fdebb8f81ef23ecb
Leeka99 <dsrn0000@gmail.com>
2025-11-14 19:19:42 +0900

    refactor: registrationReenrantLockService에서 lock을 사용하도록 개선

--- api/src/main/java/com/techcourse/api/service/javalock/ApplyServiceReentrantLock.java
+++ api/src/main/java/com/techcourse/api/service/javalock/ApplyServiceReentrantLock.java
@@ -1,11 +1,11 @@
 package com.techcourse.api.service.javalock;
 
+import com.techcourse.api.domain.entity.Course;
 import com.techcourse.api.domain.entity.Student;
+import com.techcourse.api.repository.CourseRepository;
 import com.techcourse.api.repository.StudentRepository;
 import com.techcourse.api.service.ApplyService;
 import com.techcourse.api.service.RegistrationReenrantLockService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,25 +16,16 @@ import org.springframework.stereotype.Service;
 public class ApplyServiceReentrantLock implements ApplyService {
 
     private final StudentRepository studentRepository;
-    private final ReentrantLock lock = new ReentrantLock();
+    private final CourseRepository courseRepository;
     private final RegistrationReenrantLockService registrationReenrantLockService;
 
     @Override
     public void apply(Long studentId, Long courseId) {
-        try {
-            if (!lock.tryLock(5, TimeUnit.SECONDS)) {
-                return;
-            }
-            Student student = studentRepository.findById(studentId).orElseThrow();
-
-            registrationReenrantLockService.courseRegistration(student);
-
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } finally {
-            if (lock.isHeldByCurrentThread()) { // 현재 스레드가 lock을 가지고 있는지 확인
-                lock.unlock();
-            }
-        }
+        Student student = studentRepository.findById(studentId).orElseThrow();
+        Course course = courseRepository.findAll().get(0);
+        int firstYearLimit = (int) (course.getMaxCapacity() * 0.9);
+        int otherYearLimit = (int) (course.getMaxCapacity() * 0.1);
+        registrationReenrantLockService.courseRegistration(student, course, firstYearLimit,
+            otherYearLimit);
     }
 }
