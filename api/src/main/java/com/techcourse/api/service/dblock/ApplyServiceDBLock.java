046bfd5cddf654d94b8e8caf41cb1b4530a142d8
Leeka99 <dsrn0000@gmail.com>
2025-11-14 22:14:17 +0900

    feat: 비관적 락을 이용한 수강신청 서비스 구현
    - 기존 ReentRantLcok만으로 해결할 수 없는 데드락 문제 해결

---
+++ api/src/main/java/com/techcourse/api/service/dblock/ApplyServiceDBLock.java
@@ -0,0 +1,35 @@
+package com.techcourse.api.service.dblock;
+
+import com.techcourse.api.domain.entity.Course;
+import com.techcourse.api.domain.entity.Student;
+import com.techcourse.api.repository.CourseRepository;
+import com.techcourse.api.repository.StudentRepository;
+import com.techcourse.api.service.ApplyService;
+import com.techcourse.api.service.registration.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Primary
+public class ApplyServiceDBLock implements ApplyService {
+
+    private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
+    private final RegistrationService registrationService;
+
+    @Override
+    @Transactional
+    public void apply(Long studentId, Long courseId) {
+        Student student = studentRepository.findById(studentId).orElseThrow();
+        Course course = courseRepository.findByIdWithPessimisticLock(courseId).orElseThrow();
+        int firstYearLimit = (int) (course.getMaxCapacity() * 0.9);
+        int otherYearLimit = (int) (course.getMaxCapacity() * 0.1);
+        registrationService.courseRegistration(student, course, firstYearLimit,
+            otherYearLimit);
+    }
+}
